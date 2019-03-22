86d200fe8d0cf6f81bce6cc0817553c41dc9e797
Christopher Hubmann <christopher.hubmann@gmail.com>
2019-03-22 22:33:45 +0100

    move Watchdog to own lib

---
+++ lib/Watchdog/include/Watchdog.h
@@ -0,0 +1,13 @@
+#ifndef _WATCHDOG_H_
+#define _WATCHDOG_H_
+
+class Watchdog {
+
+public:
+
+  void setup();
+  void reset();
+
+};
+
+#endif //_WATCHDOG_H_
--- src/main.cpp
+++ src/main.cpp
@@ -23,32 +23,14 @@ void hidMouseStateChange(MouseState * state) {
 // Helpers
 //
 #include "Logging.h"
+#include "Watchdog.h"
+Watchdog watchdog;
 
 //
 // Initial setup
 //
-void setupWatchdog() {
-  // use low-power 32.768kHz oscillator as clock source @ 1024Hz.
-  GCLK->GENDIV.reg = GCLK_GENDIV_ID(GCM_FDPLL96M_32K) | GCLK_GENDIV_DIV(4); // use divisor of 32=2^(x+1)|x=4
-  while (GCLK->STATUS.bit.SYNCBUSY); // wait for synchronization
-  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(GCM_FDPLL96M_32K) | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSCULP32K | GCLK_GENCTRL_DIVSEL | GCLK_GENCTRL_IDC;
-  while (GCLK->STATUS.bit.SYNCBUSY); // wait for synchronization
-  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID_WDT | GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK2; // feed GCLK2 to WDT
-  while (GCLK->STATUS.bit.SYNCBUSY); // wait for synchronization
-
-  // disable early-warning interrupt
-  NVIC_DisableIRQ(WDT_IRQn);
-  NVIC_ClearPendingIRQ(WDT_IRQn);
-  WDT->INTENSET.bit.EW = 0;
-
-  WDT->CONFIG.bit.PER = 9; // Set period to 4096=2^(x+3)|x=9|00<=x<=0B clock cycles (4s)
-  WDT->CTRL.bit.WEN = 0; // disable windowed mode (also resets on underflow)
-  WDT->CTRL.bit.ENABLE = 1; // start WDT
-  while (WDT->STATUS.bit.SYNCBUSY); // wait for synchronization
-}
-
 void setup() {
-  setupWatchdog();
+  watchdog.setup();
   Serial.begin(SERIAL_SPEED);
   ps2Mouse.reset();
   logStatus(); printLog(); // print status log on bootup
@@ -59,9 +41,7 @@ void setup() {
 //
 void loop() {
   // reset watchdog
-  if (!WDT->STATUS.bit.SYNCBUSY) {
-    WDT->CLEAR.reg = WDT_CLEAR_CLEAR_KEY;
-  }
+  watchdog.reset();
 
   // print log if someone writes to our serial input
   if (Serial.available() && Serial.read()) {
